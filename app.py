94ca9c1a3889f6014514cf8f06d2529569d387f5
Brian McK <mogvaj@yahoo.com>
2022-05-28 18:29:44 -0600

    Now starting the answer page

--- app.py
+++ app.py
@@ -7,3 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("getMetar", methods=['GET'])
+def getMetar(anIcaoCode):
+    return f"<h1>You searched for {getMetar}</h1>"
