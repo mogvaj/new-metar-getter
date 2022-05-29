b7cc0299fd2de5a0c2300eb3c6973bc53d6de35c
Brian McK <mogvaj@yahoo.com>
2022-05-28 18:36:17 -0600

    Fixing getMetar endpoint

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/getMetar", methods=['GET'])
-def getMetar(anIcaoCode):
+@app.route("/getMetar/<anIcaoCode>", methods=['GET'])
+def getMetar(anIcaoCode: str):
     return f"<h1>You searched for {getMetar}</h1>"
