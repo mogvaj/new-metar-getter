b1042077a092175535a5d17035d3f720b9f91c18
Brian McK <mogvaj@yahoo.com>
2022-05-28 18:47:04 -0600

    Commented out getMetar

--- app.py
+++ app.py
@@ -8,8 +8,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+"""
 @app.route("/getMetar/<anIcaoCode>")
 def getMetar(anIcaoCode: str):
     print (f"Starting getMetar() with an argument of {anIcaoCode}")
-    
+
     return f"<h1>You searched for {anIcaoCode}</h1>"
+"""
